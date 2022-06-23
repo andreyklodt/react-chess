004f7583a0d527ba1a8623346c601eb23e8c53f5
Andrey Klodt <andreyklodt@gmail.com>
2022-06-24 08:13:33 +1000

    Add base webpack react configuration

--- webpack.config.js
+++ webpack.config.js
@@ -6,17 +6,27 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name]-[hash].bundle.js'
+    filename: '[name]-[hash].bundle.js',
   },
   plugins: [
-    new HTMLWebpackPlugin({template: './src/index.html'}),
+    new HTMLWebpackPlugin({ template: './src/index.html' }),
   ],
   devServer: {
     static: {
-      directory: path.join(__dirname, 'public')
+      directory: path.join(__dirname, 'public'),
     },
     compress: true,
-    port: 3000
-  }
-
+    port: 3000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
 }
